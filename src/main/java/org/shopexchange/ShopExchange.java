8b47c2a1a3fca56a025b6c206c3e73b0adf0a9db
Joohnnys <redjohnnycraft@gmail.com>
2017-07-14 23:48:27 -0300

    organized DB handlers and MySQL

--- src/main/java/org/shopexchange/ShopExchange.java
+++ src/main/java/org/shopexchange/ShopExchange.java
@@ -5,10 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.shopexchange.db.DatabaseHandler;
 import org.shopexchange.db.MySQLHandler;
 import org.shopexchange.db.YmlHandler;
-import org.shopexchange.db.mysql.MySQL;
+import org.shopexchange.db.mysql.DAOConnection;
 import org.shopexchange.listeners.ChestShopListener;
-
-import net.md_5.bungee.api.ChatColor;
+import org.shopexchange.util.MessagesUtil;
 
 public class ShopExchange extends JavaPlugin {
 
@@ -32,7 +31,7 @@ public class ShopExchange extends JavaPlugin {
 			dbHandler = new YmlHandler();
 			break;
 		case "mysql":
-			MySQL.init();
+			DAOConnection.init();
 			dbHandler = new MySQLHandler();
 		}
 	}
@@ -50,6 +49,10 @@ public class ShopExchange extends JavaPlugin {
 	}
 
 	public void getLogger(String str) {
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', str));
+		Bukkit.getConsoleSender().sendMessage(MessagesUtil.setColored(str));
+	}
+	
+	public String getLang() {
+		return getConfig().getString("lang");
 	}
 }
--- src/main/java/org/shopexchange/db/MySQLHandler.java
+++ src/main/java/org/shopexchange/db/MySQLHandler.java
@@ -1,90 +1,32 @@
 package org.shopexchange.db;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.shopexchange.db.mysql.MySQL;
+import org.shopexchange.db.mysql.DAOStatement;
+import org.shopexchange.db.mysql.DAOStatement.TYPE;
 
 public class MySQLHandler implements DatabaseHandler {
 
 	@Override
 	public void setBuyValue(String key, double price) {
-		try {
-			PreparedStatement ps = MySQL.getConnection()
-					.prepareStatement("INSERT INTO Items (Key,Buy) VALUES (?,?)");
-			ps.setString(1, key);
-			ps.setDouble(2, price);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		DAOStatement.insertDouble(key, TYPE.BUY, price);
 	}
 
 	@Override
 	public void setSellValue(String key, double price) {
-		try {
-			PreparedStatement ps = MySQL.getConnection()
-					.prepareStatement("INSERT INTO Items (Key,Sell) VALUES (?,?)");
-			ps.setString(1, key);
-			ps.setDouble(2, price);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		DAOStatement.insertDouble(key, TYPE.SELL, price);
 	}
 
 	@Override
 	public double getBuyValue(String key) {
-		double value = 0;
-		try {
-			PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT Buy FROM Items WHERE Key = ?");
-			ps.setString(1, key);
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next() == true)
-				value = rs.getDouble("Buy");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return value;
+		return DAOStatement.selectDouble(key, TYPE.BUY);
 	}
 
 	@Override
 	public double getSellValue(String key) {
-		double value = 0;
-		if (keyExists(key)) {
-			try {
-				PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT Sell FROM Items WHERE Key = ?");
-				ps.setString(1, key);
-				ResultSet rs = ps.executeQuery();
-
-				if (rs.next() == true)
-					value = rs.getDouble("Sell");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return value;
+		return DAOStatement.selectDouble(key, TYPE.SELL);
 	}
 
 	public boolean keyExists(String key) {
-		boolean exists = false;
-		try {
-			PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT Sell FROM Items WHERE Key = ?");
-			ps.setString(1, key);
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next() == true)
-				exists = true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return exists;
+		return DAOStatement.keyExists(key);
 	}
 
 }
